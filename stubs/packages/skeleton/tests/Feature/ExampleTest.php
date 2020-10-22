96e0e9a0f2744929dc439ebe984f881cfe54f227
Daniel Sundermeier <daniel@hof-sundermeier.de>
2020-10-22 08:00:52 +0200

    added: skeleton tests, controller

---
+++ stubs/packages/skeleton/tests/Feature/ExampleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace D15r\Deployment\Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use D15r\Deployment\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+}
